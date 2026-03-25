861492604da5fcc4c9742d7af646113c971b9655
Augusto mbota <augustombota0@gmail.com>
2026-03-25 05:09:20 +0000

    inclusao da funcao peek e empty

--- fila/fila/src/Main.java
+++ fila/fila/src/Main.java
@@ -32,6 +32,20 @@ public class Main {
                         System.out.println("eleminar elemento: " + fila.poll());
                     else
                         System.out.println("A fila está vazia");
+                    break;
+                case 3:
+                    if(! fila.isEmpty())
+                        System.out.println("visualizar o elemento frente: " + fila.peek());
+                    else
+                         System.out.println("A fila está vazia");
+                    break;
+                case 4:
+
+                     if(fila.isEmpty())
+                         System.out.println(" Sim, a fila está vazia");
+                     else
+                         System.out.println("A fila não está vazia ");
+                     break;
             }
         }while (  opcao != 0);
     }
